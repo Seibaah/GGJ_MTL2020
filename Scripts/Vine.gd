3b86cb38f69480fd2db6f0436228c74c1feda2d2
AlxGravel <alexandre.gravel@live.ca>
2020-02-01 16:37:06 -0500

    plant hitbox

--- Scripts/Vine.gd
+++ Scripts/Vine.gd
@@ -1,14 +1,13 @@
 extends KinematicBody2D
 
-onready var anim_player = $AnimatedSprite
+onready var anim_player = $AnimationPlayer
+
 
 func _physics_process(delta):
 	#plant_seed
-	if Input.is_action_just_pressed("plant_seed"):
-		anim_player.play("growing")
-	if Input.is_action_just_released("plant_seed"):
-		anim_player.stop()
-	
+	if Input.is_action_pressed("plant_seed"):
+		anim_player.play("grow")
+		
 
 		
 		
