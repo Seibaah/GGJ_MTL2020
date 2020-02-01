0950ef553b80288788a54b7fa4db61f0e6e2cdf8
Jules Jehanno <julees.jehanno@courriel.ca>
2020-02-01 16:53:05 -0500

    Great commit v2

--- Scripts/Vine.gd
+++ Scripts/Vine.gd
@@ -1,14 +1,19 @@
 extends KinematicBody2D
 
 onready var anim_player = $AnimatedSprite
+var anim_playing = false
 
 func _physics_process(delta):
 	#plant_seed
 	if Input.is_action_just_pressed("plant_seed"):
 		anim_player.play("growing")
+		anim_playing = anim_player.is_playing()
+		#_on_AnimatedSprite_animation_finished()
 	if Input.is_action_just_released("plant_seed"):
-		anim_player.stop()
-	
+		if not anim_playing:
+			anim_player.stop()
+		
 
-		
-		
+
+func _on_AnimatedSprite_animation_finished():
+	anim_player.stop()
