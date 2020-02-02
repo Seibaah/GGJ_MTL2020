2216c91c7fa1e0ae946515d700fc403c0a30a8b4
Seibaah <dalexandre_12@outlook.com>
2020-02-02 02:03:28 -0500

    Fix world load, delete deprecated, fix main menu

--- World_complete.gd
+++ World_complete.gd
@@ -6,6 +6,7 @@ signal ask_next_level
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
-		if body.name == "Player":
+		if body.is_in_group("player"):
+			print("emmitttt")
 			emit_signal("ask_next_level")
 			#get_tree().change_scene("World-2.tscn")
