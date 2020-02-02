d871a602f7c7efd6f762e617f41286785a8fc446
AlxGravel <alexandre.gravel@live.ca>
2020-02-01 23:51:32 -0500

    in the middle of some changes

--- Scripts/Vine.gd
+++ Scripts/Vine.gd
@@ -1,12 +1,41 @@
 extends KinematicBody2D
 
+var is_planted:bool = false
+var is_withered:bool = false
 
-onready var anim_player = $AnimationPlayer
+var is_growing:bool = false
+export var plant_id:int = 0
+export var direction_growth:Vector2 = Vector2(0,0)
+export var impact_force = 50
+
+const HAS_GROWN = "has_grown"
+const HAS_WITHERED = "has_withered"
+const HAS_PUSHED_PLAYER = "has_pushed_player"
+signal has_grown
+signal has_withered
+signal has_pushed_player
 
 func _ready():
-	anim_player.play("grow")
+	grow()
 
+func _physics_process(delta):
+	if is_growing:
+		var bodies = $Area2D.get_overlapping_bodies()
+		for a_body in bodies:
+			if a_body.is_in_group("player"):
+				var pushed_player = a_body
+				emit_signal("has_pushed_player", self, pushed_player)
 
-func _on_AnimatedSprite_animation_finished():
-	anim_player.stop()
-	print("test")
+func shrug():
+	pass
+
+func wither():
+	self.hide()
+	is_growing = false
+
+func grow():
+	self.show()
+	is_growing = true
+
+func stop_growing():
+	is_growing = true
