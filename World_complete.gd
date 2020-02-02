55fefc85cc4602cb212ad67dcbdd1c3bc822f8e8
MrTaron <andre.taron@hotmail.com>
2020-02-02 01:01:33 -0500

    Changes to MainGame

--- World_complete.gd
+++ World_complete.gd
@@ -1,8 +1,11 @@
 #World_complete.gd
 extends Area2D
 
+signal ask_next_level
+
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
-			get_tree().change_scene("World-2.tscn")
+			emit_signal("ask_next_level")
+			#get_tree().change_scene("World-2.tscn")
